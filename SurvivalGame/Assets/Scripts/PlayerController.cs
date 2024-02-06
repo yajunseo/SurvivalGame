8402ec194fbecfeacb18f42325a292840c0dd304
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-02-06 23:14:12 +0900

    [서바이벌/입력/야준서] 플레이어 뛰기 움크리기 추가

--- SurvivalGame/Assets/Scripts/PlayerController.cs
+++ SurvivalGame/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float crouchSpeed;
+    private float applySpeed;
+
+
+    [SerializeField]
+    private float jumpForce;
+
+    private bool isRun = false;
+    private bool isCrouch = false;
+    private bool isGround = false;
+
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    private CapsuleCollider capsuleCollider;
+
     [SerializeField]
     private float lookSensitivity;
     [SerializeField]
@@ -21,14 +43,116 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         myRigid = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
     }
 
     // Update is called once per frame
     void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    private void TryJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if (isCrouch)
+            Crouch();
+
+        myRigid.velocity = transform.up * jumpForce;
+    }
+
+    private void TryRun()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Runnuing();
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            CancelRunnung();
+        }
+    }
+
+    private void Runnuing()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void CancelRunnung()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    private void TryCrouch()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+        
+        if(isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        Vector3 camPos = theCamera.transform.localPosition;
+
+        int count = 0;
+
+        while (_posY != applyCrouchPosY)
+        {
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(camPos.x, _posY, camPos.z);
+
+            if (count > 15)
+                break;
+
+            yield return null;
+        }
+
+        theCamera.transform.localPosition = new Vector3(camPos.x, applyCrouchPosY, camPos.z);
     }
 
     private void Move()
@@ -39,7 +163,7 @@ public class PlayerController : MonoBehaviour
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirY;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
